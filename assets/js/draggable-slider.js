6bc4615a9bc7b343a0daf415a1255859c659773b
Sama Mansour <samamansour306@gmail.com>
2023-07-30 21:30:55 +0300

    Fix sliders

--- assets/js/draggable-slider.js
+++ assets/js/draggable-slider.js
@@ -1,37 +1,24 @@
-let isDown = false;
-let startX;
-let scrollLeft;
-const slider = document.querySelector(".items");
+// Instantiate the Bootstrap carousel
+$(".multi-item-carousel").carousel({
+  interval: false,
+});
 
-const end = () => {
-  isDown = false;
-  slider.classList.remove("active");
-};
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$(".multi-item-carousel .item").each(function () {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
 
-const start = (e) => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-};
-
-const move = (e) => {
-  if (!isDown) return;
-
-  e.preventDefault();
-  const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-  const dist = x - startX;
-  slider.scrollLeft = scrollLeft - dist;
-};
-
-(() => {
-  slider.addEventListener("mousedown", start);
-  slider.addEventListener("touchstart", start);
-
-  slider.addEventListener("mousemove", move);
-  slider.addEventListener("touchmove", move);
-
-  slider.addEventListener("mouseleave", end);
-  slider.addEventListener("mouseup", end);
-  slider.addEventListener("touchend", end);
-})();
+  if (next.next().length > 0) {
+    next.next().children(":first-child").clone().appendTo($(this));
+  } else {
+    $(this)
+      .siblings(":first")
+      .children(":first-child")
+      .clone()
+      .appendTo($(this));
+  }
+});
